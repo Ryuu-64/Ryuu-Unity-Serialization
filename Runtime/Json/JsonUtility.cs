f6e2f2aa7088b3b1499fa9e08be63dd253577069
Ryuu-64 <64ryuu@gmail.com>
2024-02-29 16:18:44 +0800

    Initial commit

---
+++ Runtime/Json/JsonUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Ryuu.Unity.Serialization.Json
+{
+    public static class JsonUtility
+    {
+        public static T Deserialize<T>(string jsonString, Func<T> fallbackProvider)
+        {
+            try
+            {
+                T objectFromJsonString = JsonConvert.DeserializeObject<T>(jsonString);
+                // ReSharper disable once ConvertIfStatementToReturnStatement
+                if (objectFromJsonString != null)
+                {
+                    return objectFromJsonString;
+                }
+
+                return fallbackProvider.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Debug.LogError(exception);
+                return fallbackProvider.Invoke();
+            }
+        }
+    }
+}
